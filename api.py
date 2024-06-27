f3e17aec6df681801d469d16f3b4848e60dd5edc
Paulo C. <oluap.cesar.rocha@gmail.com>
2024-06-27 19:22:08 -0300

    feature : add flask api

---
+++ api.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host='mysql_container',
+        user='root',
+        password='root',
+        database='mysql'
+    )
+
+@app.route("/")
+def hello():
+    return "<h1><p>UFRB SA.</p></h1>"
+
+
+@app.route('/customers', methods=['GET'])
+def list_customers():
+    name = request.args.get('name')
+    address = request.args.get('address')
+    query = "SELECT * FROM clientes WHERE 1=1"
+    params = []
+
+    if name:
+        query += " AND name LIKE %s"
+        params.append(f"%{name}%")
+    if address:
+        query += " AND address LIKE %s"
+        params.append(f"%{address}%")
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, params)
+    customers = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return jsonify(customers)
+
+
+@app.route('/card/<int:card_id>/limit', methods=['GET'])
+def get_limit(card_id):
+    query = "SELECT Limite FROM cartoes WHERE CartaoID = %s"
+    params = (card_id,)
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, params)
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    if result:
+        return jsonify(result)
+    else:
+        return jsonify({"message": "Card not found"}), 404
+
+
+@app.route('/card/<int:card_id>/transactions', methods=['GET'])
+def list_transactions(card_id):
+    query = """
+    SELECT t.*
+    FROM transacoes t
+    JOIN faturas i ON t.FaturaID = i.id
+    WHERE i.CartaoID = %s AND i.MesReferencia = MONTH(now())
+    """
+    params = (card_id,)
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, params)
+    transactions = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return jsonify(transactions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
